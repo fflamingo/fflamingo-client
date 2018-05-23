bc8b8fc2d145b0cfed863bd01ba7b96ed10504d6
leonardfactory <leonardo.ascione@gmail.com>
2018-05-23 10:00:42 +0200

    Setup prettier

--- packages/ui/src/Button.spec.tsx
+++ packages/ui/src/Button.spec.tsx
@@ -1,10 +1,10 @@
-import { shallow } from "enzyme";
-import { Button } from "./Button";
-import React from "react";
+import { shallow } from 'enzyme';
+import { Button } from './Button';
+import React from 'react';
 
-describe("Button", () => {
+describe('Button', () => {
   test('should render a type="button" element', () => {
     const wrapper = shallow(<Button onClick={e => {}} text="Hello" />);
-    expect(wrapper.find("button").text()).toEqual("Hello");
+    expect(wrapper.find('button').text()).toEqual('Hello');
   });
 });
